40e5cc0e11d30ce2374f8e84ff994f99d62c29a0
macair <@Gayrat17>
2026-02-28 21:31:21 +0500

    Admin id

--- models/films.py
+++ models/films.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, ForeignKey, UniqueConstraint
+from sqlalchemy import String, Integer, ForewignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from models.base import Model
