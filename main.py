1f9969c47925e62262259d61b4058cca5dee0d4a
macair <@Gayrat17>
2026-03-02 15:50:49 +0500

    updated main.py

--- main.py
+++ main.py
@@ -3,39 +3,110 @@ import logging
 import sys
 from csv import DictReader
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, InlineKeyboardButton, BotCommand, \
-    BotCommandScopeAllPrivateChats
+    BotCommandScopeAllPrivateChats, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from config import settings
 from models.regions import Region, District
 
-
 dp = Dispatcher()
 
 
-
 class Form(StatesGroup):
-    first_name = State()
-    birthday = State()
-    image = State()
+    name = State()
 
 
-@dp.message(CommandStart())
-async def command_start(message: Message) -> None:
+def regions_btn():
     ikm = InlineKeyboardBuilder()
     text = Region.get_all()
     for region in text:
         ikm.add(InlineKeyboardButton(text=region.name, callback_data=f"region:{region.id}"))
     ikm.adjust(1)
+    return ikm
+
+
+def districts_btn(_id):
+    districts = District.filter(region_id=_id)
+    ikm = InlineKeyboardBuilder()
+    for d in districts:
+        ikm.row(InlineKeyboardButton(text=f"📍{d.name}", callback_data=f"district:{d.name}"),
+                InlineKeyboardButton(text="✏️", callback_data=f"change_districts:{d.id}"),
+                InlineKeyboardButton(text="❌", callback_data=f"remove_districts:{d.id}"))
+    ikm.row(InlineKeyboardButton(text="⬅️ back", callback_data="back"))
+    return ikm
+
+
+@dp.message(CommandStart())
+async def command_start(message: Message) -> None:
+    ikm = regions_btn()
     await message.answer('Xush kelibsiz')
     await message.answer('Viloyatlar', reply_markup=ikm.as_markup())
+
+
+@dp.callback_query(F.data.startswith("district:"))
+async def callback_query_handler(callback: CallbackQuery):
+    name = callback.data.removeprefix("district:")
+    await callback.message.answer(f"{name}ni tanladingiz!")
+
+
+
+@dp.callback_query(F.data.startswith("region:"))
+async def region_handler(callback: CallbackQuery) -> None:
+    _id = callback.data.removeprefix("region:")
+    ikm = districts_btn(_id)
+    await callback.message.edit_text("Tumanlardan birini tanlang:", reply_markup=ikm.as_markup())
+
+
+@dp.callback_query(F.data == "back")
+async def back_handler(callback: CallbackQuery):
+    ikm = regions_btn()
+    await callback.message.edit_text('Viloyatlar', reply_markup=ikm.as_markup())
+
+
+@dp.callback_query(F.data.startswith("change_districts:"))
+async def back_handler(callback: CallbackQuery, state: FSMContext):
+    _id = callback.data.removeprefix("change_districts:")
+    await state.update_data(districts_id=_id)
+    await state.set_state(Form.name)
+    await callback.message.answer("Yangi nom kiriting: ")
+
+
+@dp.message(Form.name)
+async def change_name_handler(message: Message, state: FSMContext):
+    data = await state.get_data()
+    District.update(data['districts_id'], name=message.text)
+    await state.clear()
+    await message.answer("Nomi o'zgardi", show_alert=True)
+
+
+@dp.callback_query(F.data.startswith("remove_districts:"))
+async def back_handler(callback: CallbackQuery):
+    _id = callback.data.removeprefix("remove_districts:")
+    district = District.delete(_id)
+    await callback.answer(f"{district.name} bazadan o'chirildi", show_alert=True)
+    ikm = districts_btn(district.region_id)
+    await callback.message.edit_text('Tumanlar', reply_markup=ikm.as_markup())
+
+@dp.message()
+async def forward_to_admin(message: Message):
+    user = message.from_user
+
+    text = (
+        f"📩 Yangi xabar!\n\n"
+        f"👤 Ism: {user.full_name}\n"
+        f"🆔 ID: {user.id}\n"
+        f"📨 Xabar:\n{message.text}"
+    )
+
+    await message.bot.send_message(settings.ADMIN_ID, text)
 
 
 @dp.message(Command("migrate"))
@@ -80,6 +151,7 @@ async def command_start_handler(message: Message, bot: Bot) -> None:
 
 
 async def startup(bot: Bot) -> None:
+    # await bot.set_my_description("🤖 My first bot!\n©️ Xushboqov G'ayratjon")
     await bot.set_my_commands([
         BotCommand(command='start', description='Boshlash'),
         BotCommand(command='migrate', description='Databasega yozish'),
